d7ff25a45f1ef1f9bff682b0a44219ea7fe12cf8
sinnoorc <sinnoorc@gmail.com>
2025-01-03 10:52:02 +0530

    refactor figma_squircle library exports and clean up SmoothBorderRadius
    - Added export for 'src/clip_smooth_rect.dart' in figma_squircle.dart.
    - Removed unnecessary commented-out code in SmoothBorderRadius class to improve code clarity.

--- lib/figma_squircle.dart
+++ lib/figma_squircle.dart
@@ -1,6 +1,6 @@
 library figma_squircle;
 
+export 'src/clip_smooth_rect.dart';
 export 'src/smooth_border_radius.dart';
 export 'src/smooth_radius.dart';
 export 'src/smooth_rectangle_border.dart';
-export 'src/clip_smooth_rect.dart';
